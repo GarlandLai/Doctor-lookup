865a8b50f3ba4717973f114db019fa6a82e8109d
Garland Lai <garlandlai24@gmail.com>
2019-07-05 09:22:21 -0700

    add some logic to UI and business

--- src/doctor.js
+++ src/doctor.js
@@ -1,1 +1,19 @@
+export class DoctorLook {
+  getDoctor(medIssue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
 
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d8dd66e6a045597f1ce5f28b0149c8e9`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import $ from 'jquery';
 import { } from './doctor.js'
 
-
 $(document).ready(function() {
+  $('#findDoc1')click(function(){
+    let nameInput = $('#name').val();
+    let doctorLook = new DoctorLook();
+
+    let promise = doctorLook.getDoctor(name);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.output').text();
+  });
+
+
+
+
+
+
+//   $('#findDoc2')click(function() {
+//     let issueInput = $('#medIssue').val();
+//     $('#medIssue').val("");
+//   });
+// });
