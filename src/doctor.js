0ae46a03b2caf0ed250e33c6b52eda175c00a180
Garland Lai <garlandlai24@gmail.com>
2019-07-07 16:55:18 -0700

    issues with API. Currently able to print some output, but not when query is searched

--- src/doctor.js
+++ src/doctor.js
@@ -1,9 +1,9 @@
 export class DoctorLook {
-  getDoctor(firstName, lastName, issueInput) {
+  getDoctor(userInput) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
 
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155,-122.6793,100&user_key=a0220cf0dee3060d82e4f322141749b6`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=50&user_key=a0220cf0dee3060d82e4f322141749b6`;
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -6,22 +6,21 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#findDoc').click(function(){
-    let firstName = $('#firstName').val();
-    $('#firstName').val("");
-    let lastName = $('#lastName').val();
-    $('#lastName').val("");
-    let issueInput = $('#medIssue').val();
-    $('#medIssue').val("");
+    let userInput = $('#input').val();
+    $('#input').val("");
+    // let issueInput = $('#medIssue').val();
+    // $('#medIssue').val("");
     let doctorLook = new DoctorLook();
 
-    let promise = doctorLook.getDoctor(firstName, lastName, issueInput );
+    let promise = doctorLook.getDoctor(userInput);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       console.log(body);
-      for (let i = 0; body.data.length <= 0; i+=1) {
-        if (body.data[i].profile.last_name.includes(lastName) || body.data[i].profile.first_name.includes(firstName)) {
-          console.log(body.data[i].profile.first_name);
+      for (let i = 0; i < body.data.length; i+=1) {
+        console.log(body.data.length)
+        if (body.data[i].profile.first_name.includes(userInput) || body.data[i].profile.last_name.includes(userInput) || body.data[i].profile.specialties.includes(userInput)) {
+          $('.output ul').append(`<li> Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, Address: ${body.data[i].practices.visit_adresss}, Phone: ${body.data[i].practices.phones}, Accepting New Patients: ${body.data[i].practices.accepts_new_patients}  </li>`);
           // $('.output').append(body.data[i].lastName, firstName, address, phone number, website, accepting new patients)
         // } else {
         //   $('.output').text('There are 0 results matching your search. Please update or check your search. Thank you!')
