c0ea0c841486452872b8eab444b7ba9306be2859
Garland Lai <garlandlai24@gmail.com>
2019-07-07 20:01:48 -0700

    switch back with API in JS doc

--- src/doctor.js
+++ src/doctor.js
@@ -17,7 +17,7 @@ export class DoctorLook {
   getDoctorName(nameInput) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameInput}&location=or-portland&skip=0&limit=20&user_key=process.exports.apiKey`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameInput}&location=or-portland&skip=0&limit=20&user_key=a0220cf0dee3060d82e4f322141749b6`;
 
       request.onload = function() {
         if (this.status === 200) {
