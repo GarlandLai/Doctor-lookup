d271b31f6b7d4c0e58eceae4a8e31a47ad875b2c
Garland Lai <garlandlai24@gmail.com>
2019-07-07 18:08:06 -0700

    add error for promise

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import './styles.css';
 $(document).ready(function() {
   $('#findDoc').click(function(){
     const userInput = $('#input').val();
-    // $('#input').val("");
     let doctorLook = new DoctorLook();
     let promise = doctorLook.getDoctor(userInput);
 
@@ -16,14 +15,13 @@ $(document).ready(function() {
       console.log(body);
       if (body.data.length === 0) {
         $('.output ul').html(' <li> No results have been found. please check your search and try again. </li>')
-      }
+      };
 
       for (let i = 0; i < body.data.length; i+=1) {
-          $('.output ul').append(`<li> Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, Address: ${body.data[i].practices[0].visit_address.street}, Phone: ${body.data[i].practices[0].phones[0].number}, Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`);
-        }
+          $('.output ol').append(`<li> Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, Address: ${body.data[i].practices[0].visit_address.street}, Phone: ${body.data[i].practices[0].phones[0].number}, Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`);
+        };
+      }, function (error) {
+          $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
-    // function (error) {
-    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    // });
   });
 });
