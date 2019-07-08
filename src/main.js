5a8c28fd2671b84538779002ba8b2bce4a8ac0ad
Garland Lai <garlandlai24@gmail.com>
2019-07-07 17:37:15 -0700

    moved the if statment above for loop. working correctly if array === 0, but for loop still doesnt like the includes()

--- src/main.js
+++ src/main.js
@@ -6,24 +6,26 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#findDoc').click(function(){
-    let userInput = $('#input').val();
-    $('#input').val("");
-    // let issueInput = $('#medIssue').val();
-    // $('#medIssue').val("");
+    const userInput = $('#input').val();
+    // $('#input').val("");
     let doctorLook = new DoctorLook();
-
     let promise = doctorLook.getDoctor(userInput);
 
     promise.then(function(response) {
-      let body = JSON.parse(response);
+      const body = JSON.parse(response);
       console.log(body);
+      if (body.data.length === 0) {
+        $('.output ul').html(' <li> No results have been found. please check your search and try again. </li>')
+      }
+
       for (let i = 0; i < body.data.length; i+=1) {
-        console.log(body.data.length)
         if (body.data[i].profile.first_name.includes(userInput) || body.data[i].profile.last_name.includes(userInput) || body.data[i].profile.specialties.includes(userInput)) {
           $('.output ul').append(`<li> Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, Address: ${body.data[i].practices[0].visit_address.street}, Phone: ${body.data[i].practices[0].phones[0].number}, Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`);
         }
       }
-
     });
+    // function (error) {
+    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    // });
   });
 });
