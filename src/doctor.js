0073e4c93625c7a059ececbf5e0cb9c1aecfe1b2
Garland Lai <garlandlai24@gmail.com>
2019-07-07 19:32:15 -0700

    add another API and logic for name search

--- src/doctor.js
+++ src/doctor.js
@@ -3,7 +3,24 @@ export class DoctorLook {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
 
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=50&user_key=a0220cf0dee3060d82e4f322141749b6`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=20&user_key=a0220cf0dee3060d82e4f322141749b6`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  getDoctorName(nameInput) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameInput}&location=or-portland&skip=0&limit=20&user_key=a0220cf0dee3060d82e4f322141749b6`;
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -7,20 +7,51 @@ import './styles.css';
 $(document).ready(function() {
   $('#findDoc').click(function(){
     const userInput = $('#input').val();
+    $('#input').val('');
+    $('.output ol').empty();
+    $('#noResults').empty();
     let doctorLook = new DoctorLook();
     let promise = doctorLook.getDoctor(userInput);
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
       if (body.data.length === 0) {
-        $('.output ul').html(' <li> No results have been found. please check your search and try again. </li>')
+        $('.output ol').html(' <h5>No results have been found. please check your search and try again.</strong><h5>')
       };
       for (let i = 0; i < body.data.length; i+=1) {
-        $('.output ol').append(`<li> Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, Address: ${body.data[i].practices[0].visit_address.street}, Phone: ${body.data[i].practices[0].phones[0].number}, Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}</li>`);
+        $('.output ol').append(`<li><strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</strong><br>
+          <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
+          <strong>Phone:</strong>${body.data[i].practices[0].phones[0].number}<br> <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}</li>`);
       };
     }, function (error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
+
+
+  $('#findDoc2').click(function(){
+    const nameInput = $('#name').val();
+    $('#name').val('');
+    $('.nameOutput ol').empty();
+    $('#noResults').empty();
+    let doctorLook = new DoctorLook();
+    let promise = doctorLook.getDoctorName(nameInput);
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      console.log(body);
+      if (body.data.length === 0) {
+        $('.nameOutput ol').html(' <h5> No results have been found. please check your search and try again. </h5>')
+      };
+      for (let i = 0; i < body.data.length; i+=1) {
+        $('.nameOutput ol').append(`<li><strong>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</strong><br>
+          <strong>Address:</strong> ${body.data[i].practices[0].visit_address.street}<br>
+          <strong>Phone:</strong>${body.data[i].practices[0].phones[0].number}<br> <strong>Accepting New Patients:</strong> ${body.data[i].practices[0].accepts_new_patients}</li>`);
+      };
+    }, function (error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+
 });
